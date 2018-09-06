a8999b35c6a24c350d20183662efd50538337c1e
Derek Reed <1331derek@gmail.com>
2018-09-05 19:42:03 -0600

    stuff

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     defaultLayout: "main"
+//   })
+// );
+// app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
