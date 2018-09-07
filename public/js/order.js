f746ec3cd77a3c4a6af90479a5606d92328aabf7
Derek Reed <1331derek@gmail.com>
2018-09-07 16:24:14 -0600

    ordering

--- public/js/order.js
+++ public/js/order.js
@@ -1,33 +1,87 @@
+
+
+var newOrder = {};
+
 $(".submit").on("click", function (event) {
     event.preventDefault();
 
-    var newOrder = {
-        bread: $(".bread:checked").val().trim(),
-        meat: $(".meat:checked").map(function () {
-            return this.value;
-        }).get().join(","),
-        meat: $(".meat:checked").map(function () {
-            return this.value;
-        }).get().join(","),
-        cheese: $(".cheese:checked").map(function () {
-            return this.value;
-        }).get().join(","),
-        veggies: $(".veggies:checked").map(function () {
-            return this.value;
-        }).get().join(","),
-        condiments: $(".condiments:checked").map(function () {
-            return this.value;
-        }).get().join(","),
-        chips: $(".chips:checked").map(function () {
-            return this.value;
-        }).get().join(","),
-        drinks: $(".drinks:checked").map(function () {
-            return this.value;
-        }).get().join(","),
-        
-    }
-    
+    // var newOrder = {
+    //     bread: $(".bread:checked").val().trim(),
+    //     meat: $(".meat:checked").map(function () {
+    //         return this.value;
+    //     }).get().join(","),
+    //     cheese: $(".cheese:checked").map(function () {
+    //         return this.value;
+    //     }).get().join(","),
+    //     veggies: $(".veggies:checked").map(function () {
+    //         return this.value;
+    //     }).get().join(","),
+    //     condiments: $(".condiments:checked").map(function () {
+    //         return this.value;
+    //     }).get().join(","),
+    //     chips: $(".chips:checked").map(function () {
+    //         return this.value;
+    //     }).get().join(","),
+    //     drinks: $(".drinks:checked").map(function () {
+    //         return this.value;
+    //     }).get().join(","),
 
+    // };
+
+    $.ajax("/api/orders", {
+        type: "POST",
+        data: newOrder
+    }).then(
+        function () {
+            console.log(newOrder);
+        }
+    )
+});
+
+$(".addSandwich").on("click", function (event) {
+    event.preventDefault();
+
+        newOrder.bread = $(".bread:checked").val().trim();
+        newOrder.meat = $(".meat:checked").map(function () {
+            return this.value;
+        }).get().join(",");
+        newOrder.cheese = $(".cheese:checked").map(function () {
+            return this.value;
+        }).get().join(",");
+        newOrder.veggies = $(".veggies:checked").map(function () {
+            return this.value;
+        }).get().join(",");
+        newOrder.condiments = $(".condiments:checked").map(function () {
+            return this.value;
+        }).get().join(",");
 
     console.log(newOrder);
-})
+});
+
+
+$(".addChips").on("click", function (event) {
+    event.preventDefault();
+
+    var addChips = {
+        chips: $(".chips:checked").map(function () {
+            return this.value;
+        }).get().join(",")
+    };
+    newOrder.push(addChips);
+
+    console.log(newOrder);
+});
+
+
+$(".addDrinks").on("click", function (event) {
+    event.preventDefault();
+
+    var addDrinks = {
+        drinks: $(".drinks:checked").map(function () {
+            return this.value;
+        }).get().join(",")
+    };
+    newOrder.push(addDrinks);
+
+    console.log(newOrder);
+});
